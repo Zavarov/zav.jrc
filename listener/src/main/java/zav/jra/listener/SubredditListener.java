148b80833d321a9a96fd9a5efe005d3307f3394e
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-04-03 16:31:09 +0200

    Signature of newLink requires a subreddit.

--- listener/src/main/java/zav/jra/listener/SubredditListener.java
+++ listener/src/main/java/zav/jra/listener/SubredditListener.java
@@ -1,11 +1,12 @@
 package zav.jra.listener;
 
 import zav.jra.Link;
+import zav.jra.Subreddit;
 
 import javax.annotation.Nonnull;
 import java.util.EventListener;
 
 public interface SubredditListener extends EventListener {
-    default void newLink(@Nonnull Link link){
+    default void newLink(@Nonnull Subreddit subreddit, @Nonnull Link link){
     }
 }
--- listener/src/main/java/zav/jra/observer/SubredditObserver.java
+++ listener/src/main/java/zav/jra/observer/SubredditObserver.java
@@ -12,10 +12,13 @@ import java.util.Collection;
 public class SubredditObserver extends AbstractObserver<SubredditListener>{
     @Nonnull
     private final LinkRequester requester;
+    @Nonnull
+    private final Subreddit subreddit;
     @Nullable
     private Collection<? extends Link> history;
 
     public SubredditObserver(@Nonnull Subreddit subreddit){
+        this.subreddit = subreddit;
         this.requester = new LinkRequester(subreddit);
     }
 
@@ -30,6 +33,6 @@ public class SubredditObserver extends AbstractObserver<SubredditListener>{
     public void notifyListener(SubredditListener listener) {
         //History may be null when a listener is called explicitly instead of via notifyAllListener.
         history = history == null ? requester.next() : history;
-        history.forEach(listener::newLink);
+        history.forEach(link -> listener.newLink(subreddit, link));
     }
 }
