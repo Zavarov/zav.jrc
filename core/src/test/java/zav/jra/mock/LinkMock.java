d818cc1ea894a238bda5a1d3e21141d3201ab2bd
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-04-03 12:22:06 +0200

    Move tests from 'proj/zav/*' to 'zav/*'

--- core/src/test/java/zav/jra/mock/LinkMock.java
+++ core/src/test/java/zav/jra/mock/LinkMock.java
@@ -4,8 +4,13 @@ import zav.jra.models.AbstractLink;
 import zav.jra.models.Kind;
 import zav.jra.models.Thing;
 import zav.jra.models._json.JSONAbstractLink;
+import zav.jra.models._json.JSONThing;
 
 public class LinkMock extends AbstractLink {
+    public static LinkMock from(String source){
+        return from(JSONThing.fromJson(source));
+    }
+
     public static LinkMock from(Thing thing){
         assert thing.getKind() == Kind.Link;
 
