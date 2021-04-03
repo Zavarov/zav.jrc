9f45da034f55739d17fb02d7c01f3953d1bc6ffc
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-04-03 16:35:19 +0200

    Update signature of 'newLink'.

--- listener/src/test/java/zav/jra/mock/SubredditListenerMock.java
+++ listener/src/test/java/zav/jra/mock/SubredditListenerMock.java
@@ -1,12 +1,14 @@
 package zav.jra.mock;
 
-import org.jetbrains.annotations.NotNull;
 import zav.jra.Link;
+import zav.jra.Subreddit;
 import zav.jra.listener.SubredditListener;
+
+import javax.annotation.Nonnull;
 
 public class SubredditListenerMock implements SubredditListener {
     @Override
-    public void newLink(@NotNull Link link) {
+    public void newLink(@Nonnull Subreddit subreddit, @Nonnull Link link) {
         System.out.println("\t"+link.getTitle());
     }
 }
