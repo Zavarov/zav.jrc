d6a556ae8bf4f99298fcab70241a5d510620dc56
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-12-25 10:20:45 +0100

    Modularize zav.jrc.client.

---
+++ zav.jrc.client/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module zav.jrc.client {
+  requires static org.eclipse.jdt.annotation;
+
+  requires com.fasterxml.jackson.databind;
+  requires com.google.common;
+  requires com.google.guice;
+  requires java.inject;
+  requires okhttp3;
+  requires org.apache.logging.log4j;
+  
+  requires zav.jrc.databind;
+  requires zav.jrc.endpoint;
+  requires zav.jrc.http;
+  
+  exports zav.jrc.client;
+  exports zav.jrc.client.guice;
+}
