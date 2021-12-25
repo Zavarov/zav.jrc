5d006bd600b61277c1516aacf7de13869f2365b7
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-12-25 10:24:23 +0100

    Modularize zav.jrc.api.

---
+++ zav.jrc.api/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module zav.jrc.api {
+  requires static org.eclipse.jdt.annotation;
+  
+  requires com.fasterxml.jackson.databind;
+  requires com.google.guice;
+  requires com.google.guice.extensions.assistedinject;
+  requires java.inject;
+  requires okhttp3;
+  
+  requires zav.jrc.databind;
+  requires zav.jrc.client;
+  requires zav.jrc.endpoint;
+  requires zav.jrc.http;
+  
+  requires java.desktop;
+  
+  exports zav.jrc.api;
+  exports zav.jrc.api.guice;
+}
