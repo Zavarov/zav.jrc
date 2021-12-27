9ffed10688c76fad98ff3675309859088722e458
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-12-27 15:04:28 +0100

    Create a Reddit class to act as an entry point for the other API classes. This should prevent users from having to use the Factory methods.

---
+++ zav.jrc.api/src/main/java/zav/jrc/api/Reddit.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Zavarov.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package zav.jrc.api;
+
+import com.google.inject.Injector;
+import zav.jrc.api.guice.AccountFactory;
+import zav.jrc.api.guice.SubredditFactory;
+
+import javax.inject.Inject;
+
+public class Reddit {
+  @Inject
+  private Injector injector;
+  
+  public FrontPage getFrontPage() {
+    return injector.getInstance(FrontPage.class);
+  }
+  
+  public Account getAccount(String accountName) {
+    return injector.getInstance(AccountFactory.class).create(accountName);
+  }
+  
+  public SelfAccount getSelfAccount() {
+    return injector.getInstance(SelfAccount.class);
+  }
+  
+  public Subreddit getSubreddit(String subredditName) {
+    return injector.getInstance(SubredditFactory.class).create(subredditName);
+  }
+}
