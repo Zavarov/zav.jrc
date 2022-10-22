a5cb00ca9cf1060786cadb0de1b76d820edfb08a
Zavarov <25961215+Zavarov@users.noreply.github.com>
2022-10-22 11:17:52 +0200

    Remove 'module-info.java' files.

--- zav.jrc.api/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module zav.jrc.api {
-  requires static org.eclipse.jdt.annotation;
-  
-  requires com.fasterxml.jackson.databind;
-  requires zav.jrc.databind;
-  
-  exports zav.jrc.api;
-  exports zav.jrc.api.endpoint;
-}
